29ed1ec60e5cba3e83b50f317b8ff5be3e6a43fc
George Clarke <george_clarke98@live.co.uk>
2020-03-03 15:37:15 +0000

    update

--- extractor/cleandata.py
+++
@@ -1,2 +0,0 @@
-from docopt import docopt
-
--- extractor/testcsv.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-import pandas
-def SqltoCsv():
-    con = sqlite3.connect('/Users/jay/Desktop/extra_sqlite/history.db')
-    table = pandas.read_sql('select * from calendar_info', con)
-    table.to_csv('/Users/jay/Desktop/extra_sqlite/output.csv')
-
-SqltoCsv()
