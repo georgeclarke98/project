f56b9866eee098d01b13a7622b417e8b298cca93
George Clarke <george_clarke98@live.co.uk>
2020-03-14 18:52:25 +0000

    multiprocessing

--- callirhoe/creator.py
+++ callirhoe/creator.py
@@ -10,6 +10,7 @@ import pandas
 import random
 import time
 import urllib
+import multiprocessing as mp
 
 def readCSV(month, filename, mode):
     """
@@ -26,6 +27,7 @@ def readCSV(month, filename, mode):
         for row in rows:
             print("\tOption " + str(count + 1) + ":")
             print("\t\t" + row["Title"])
+            print("\t\tMedia Link: " + row["Media"])
             count = count + 1
         #get the choice from the user
         choice = raw_input("\n\t Please choose event for month " + str(month) + ": ")
@@ -65,16 +67,17 @@ def createQR(month, urlQr):
     qrImage = qrcode.make(urlQr)
     qrImage.save("Events/qr" + str(month) + ".png")
 
-def createTop(month, currentPhoto, currentText, currentQR):
+def createTop(month, currentPhoto, currentText, currentQR, title):
     """
         Create the top portion of the calendar, this stitches the current
         event image onto a self created background for the top half, then
         adds the text for that event next to the image. It then also adds
         the qr code for the
     """
-    print("\tCreating Top Half")
+
     #make the photo for the current month fit into a size suitable for
     #the calendar
+
     os.system("convert " + currentPhoto + " -resize 940x1000" +
              " Resources/resized" + str(month) + ".png")
 
@@ -91,14 +94,19 @@ def createTop(month, currentPhoto, currentText, currentQR):
               str(month) + "-1.jpg -gravity northwest -geometry +980+150" +
               " -composite Resources/outputWithText" + str(month) + ".jpg")
 
+    #Add the title to the top
+    os.system("convert Resources/outputWithText" + str(month) + ".jpg -pointsize" +
+              " 60 -size 1500x60 -fill purple1 -stroke purple1 -gravity North -draw " +
+              "\"text 0,10 '"+ title + "'\" Resources/outputWithText" + str(month) + ".jpg")
+
     #add the qr code to the calendar
-    print("\tAdding QR code")
+
     os.system("convert " + currentQR + " -resize 150x150 " + currentQR)
     os.system("convert Resources/outputWithText" + str(month) + ".jpg " +
               currentQR + " -gravity northeast -geometry -5-5 -composite " +
               "Resources/outputWithText" + str(month) + ".jpg")
 
-def createPDF(month, CalendarName):
+def createPDF(CalendarName):
     """
         Append the top portion of the calendar to the calendar dates of
         the bottom part. Then create a new pdf of the calendar (if the
@@ -106,23 +114,72 @@ def createPDF(month, CalendarName):
         pdf
     """
 
+    print("\tAdding to PDF")
+
+    os.system("convert -compress none HistoryCalendar/Calendar* HistoryCalendar/Calendar.tiff")
+    os.system("convert -compress jpeg HistoryCalendar/Calendar.tiff " + CalendarName + ".pdf")
+    os.system("rm -r HistoryCalendar/Calendar.tiff")
+
+    #combine all the months into one printable pdf file
+    # if month == 1:
+    #     os.system("convert HistoryCalendar/Calender" + str(month) +
+    #             ".jpg " + CalendarName + ".pdf")
+    # else:
+    #     os.system("convert " + CalendarName + ".pdf HistoryCalendar/Calender" +
+    #             str(month) + ".jpg " + CalendarName + ".pdf")
+    return True
+
+def createCal(month, monthInfo, background, CalendarName, CalendarTitle):
+    """
+        Creates the actual calendar
+    """
+    print("\tCreating Calendar components for month " + str(month))
+
+    writeHol = open("holidays/event.dat", "w")
+    date = monthInfo[month - 1]["Date"]
+    writeHol.write("d|%02d%s|Event Today||normal" % (month, date[0:2]))
+    writeHol.close()
+    #create the event as a holiday so that it displays on the Date
+
+    calmagick.main_program(background, month, True)
+    #create the calendar of the current month
+
+    downloadImage(month, monthInfo[month - 1]["Media"])
+    createQR(month, monthInfo[month - 1]["Source URL"])
+    #get the photo for that event and create qr code
+
+    currentPhoto = "Events/Event" + str(month) + ".jpg"
+    writeText = open("Events/Event" + str(month) + ".txt", "w")
+    currentTextInfo = (monthInfo[month - 1]["Title"] + "\n\n" + monthInfo[month - 1]["Date"] +
+                  "\n\n" + monthInfo[month - 1]["Description"])
+    writeText.write(currentTextInfo)
+    writeText.close()
+    currentText = "Events/Event" + str(month) + ".txt"
+    currentQR = "Events/qr" + str(month) + ".png"
+    #get the saved event details
+
+    createTop(month, currentPhoto, currentText, currentQR, CalendarTitle)
+    #create the top part of the calendar with text, image
+    #and qr code
     #combine the photo and text portion to the calendar for month
-    print("\tCombining Dates and Event")
+
     os.system("convert -append Resources/outputWithText" + str(month) +
               ".jpg HistoryCalendar/HistoryCalendar" + str(month)
-              + ".jpg HistoryCalendar/Calender" + str(month) + ".jpg")
+              + ".jpg HistoryCalendar/Calendar%02d.jpg" % month)
 
-    print("\tAdding to PDF\n")
-    #combine all the months into one printable pdf file
-    if month == 1:
-        os.system("convert HistoryCalendar/Calender" + str(month) +
-                ".jpg " + CalendarName + ".pdf")
-    else:
-        os.system("convert " + CalendarName + ".pdf HistoryCalendar/Calender" +
-                str(month) + ".jpg " + CalendarName + ".pdf")
+    return True
 
 def main_program():
-    CalendarName = raw_input("What would you like to call your calendar?: ")
+    CalendarName = raw_input("What would you like to call your calendar? " +
+                   ("(Please ensure there are no spaces in the name e.g TestCalendar): "))
+    titleChoice = raw_input("Would you like your own title? (y/n): ")
+    while titleChoice != 'y' and titleChoice != 'n':
+        titleChoice = raw_input("Please answer 'y' or 'n': ")
+    if titleChoice == 'y':
+        CalendarTitle = raw_input("What would you like the title of the calendar to be?: ")
+    else:
+        CalendarTitle = "The History Of Manchester"
+    #Ask for thre name of the calendar and the title
     csvFile = sys.argv[1]
     background = "Resources/background.jpg"
 
@@ -135,44 +192,19 @@ def main_program():
         mode = "random"
     #choose the mode
 
+    monthInfo = []
     for month in range(1, 13):
-      print("For Month " + str(month) + ":")
+        monthInfo.append(readCSV(month, csvFile, mode))
 
-      print("\tGathering Event photo and Text")
-      monthInfo = readCSV(month, csvFile, mode)
-      #retreive the info of a random event for the current month
+    pool = mp.Pool(mp.cpu_count())
+    result = [pool.apply_async(createCal,args=(month, monthInfo, background, CalendarName, CalendarTitle)) for month in range(1,13)]
+    pool.close()
+    pool.join()
+    #call the create calendar and parallelise the process
+    #asynchronously apply the process for this part as nothing is shared
 
-      writeHol = open("holidays/event.dat", "w")
-      date = monthInfo["Date"]
-      writeHol.write("d|%02d%s|Event Today||normal" % (month, date[0:2]))
-      writeHol.close()
-      #create the event as a holiday so that it displays on the Date
-
-      print("\tCreating Calendar Dates")
-      calmagick.main_program(background, month, True)
-      #create the calendar of the current month
-
-      downloadImage(month, monthInfo["Media"])
-      createQR(month, monthInfo["Source URL"])
-      #get the photo for that event and create qr code
-
-      currentPhoto = "Events/Event" + str(month) + ".jpg"
-      writeText = open("Events/Event" + str(month) + ".txt", "w")
-      currentTextInfo = (monthInfo["Title"] + "\n\n" + monthInfo["Date"] +
-                    "\n\n" + monthInfo["Description"])
-      writeText.write(currentTextInfo)
-      writeText.close()
-      currentText = "Events/Event" + str(month) + ".txt"
-      currentQR = "Events/qr" + str(month) + ".png"
-      #get the saved event details
-
-      createTop(month, currentPhoto, currentText, currentQR)
-      #create the top part of the calendar with text, image
-      #and qr code
-
-      createPDF(month, CalendarName)
-      #combine the calendar and top half to create the pdf
-
+    createPDF(CalendarName)
+    #Create the pdf from the indicudual components
 
 if __name__ == "__main__":
     try:
